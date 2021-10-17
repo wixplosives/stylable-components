288b260473fe6c4129f7d5b40017846635bc89f3
Nadav Abrahami <nadav_ab@wix.com>
2021-10-17 16:25:43 +0300

    work on scenario tests

--- packages/components/src/hooks/simulations/use-scroll/use-scroll-horizontal-window.sim.tsx
+++ packages/components/src/hooks/simulations/use-scroll/use-scroll-horizontal-window.sim.tsx
@@ -1,22 +1,40 @@
 import { createSimulation } from '@wixc3/react-simulation';
-import { scenarioMixin, scrollAction } from '../../../simulation-mixins/scenario';
+import { expect } from 'chai';
+import {
+    expectElement,
+    expectElementText,
+    maxScroll,
+    scenarioMixin,
+    scrollAction,
+} from '../../../simulation-mixins/scenario';
 import { ScrollHookSimulator } from './scroll-hook-simulator';
 
 export default createSimulation({
-  componentType: ScrollHookSimulator,
-  name: 'use scroll window horizontal',
-  props: {
-    isHorizontal: true,
-  },
-  environmentProps: {
-    canvasWidth: 2000,
-    canvasHeight: 500,
-    windowWidth: 500,
-    windowHeight: 640,
-  },
-  plugins: [
-    scenarioMixin.use({
-      events: [scrollAction(300, false), scrollAction(200, false), scrollAction(-1, false), scrollAction(0, false)],
-    }),
-  ],
+    componentType: ScrollHookSimulator,
+    name: 'use scroll window horizontal',
+    props: {
+        isHorizontal: true,
+    },
+    environmentProps: {
+        canvasWidth: 2000,
+        canvasHeight: 500,
+        windowWidth: 500,
+        windowHeight: 640,
+    },
+    plugins: [
+        scenarioMixin.use({
+            title: 'scrolls horiontaly',
+            events: [
+                scrollAction(200, false),
+                expectElementText('#res', '200'),
+                scrollAction(-1, false),
+                expectElement('#res', (el: HTMLElement) => {
+                    expect(Math.round(parseFloat(el.innerText))).to.equal(Math.round(maxScroll(window, false)));
+                }),
+                scrollAction(0, false),
+                expectElementText('#res', '0'),
+            ],
+            timeout: 3000,
+        }),
+    ],
 });
--- packages/components/src/hooks/simulations/use-scroll/use-scroll-with-ref.sim.tsx
+++ packages/components/src/hooks/simulations/use-scroll/use-scroll-with-ref.sim.tsx
@@ -1,27 +1,42 @@
 import { createSimulation } from '@wixc3/react-simulation';
-import { scenarioMixin, scrollAction } from '../../../simulation-mixins/scenario';
+import { expect } from 'chai';
+import {
+    expectElements,
+    expectElementText,
+    maxScroll,
+    scenarioMixin,
+    scrollAction,
+} from '../../../simulation-mixins/scenario';
 import { ScrollHookSimulator } from './scroll-hook-simulator';
 
 export default createSimulation({
-  componentType: ScrollHookSimulator,
-  name: 'use scroll vertical with ref',
-  props: {
-    useWindowScroll: false,
-  },
-  environmentProps: {
-    canvasWidth: 200,
-    canvasHeight: 500,
-    windowWidth: 500,
-    windowHeight: 640,
-  },
-  plugins: [
-    scenarioMixin.use({
-      events: [
-        scrollAction(300, true, '#scroll-div'),
-        scrollAction(200, true, '#scroll-div'),
-        scrollAction(-1, true, '#scroll-div'),
-        scrollAction(0, true, '#scroll-div'),
-      ],
-    }),
-  ],
+    componentType: ScrollHookSimulator,
+    name: 'use scroll vertical with ref',
+    props: {
+        useWindowScroll: false,
+    },
+    environmentProps: {
+        canvasWidth: 200,
+        canvasHeight: 500,
+        windowWidth: 500,
+        windowHeight: 640,
+    },
+    plugins: [
+        scenarioMixin.use({
+            events: [
+                scrollAction(300, true, '#scroll-div'),
+                expectElementText('#res', '300'),
+                scrollAction(200, true, '#scroll-div'),
+                expectElementText('#res', '200'),
+                scrollAction(-1, true, '#scroll-div'),
+                expectElements(['#res', '#scroll-div'], (elements) => {
+                    expect(Math.round(parseFloat((elements['#res'] as HTMLElement).innerText))).to.equal(
+                        Math.round(maxScroll(elements['#scroll-div'], true))
+                    );
+                }),
+                scrollAction(0, true, '#scroll-div'),
+            ],
+            timeout: 4000,
+        }),
+    ],
 });
