af7da07531cce1f8ea538c768a7dd238ad2ed83f
Nadav Abrahami <nadav_ab@wix.com>
2021-10-19 16:29:26 +0300

    moved icons

---
+++ packages/components/src/icons/css.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IconFactory } from './icon';
+
+export const CssIcon = IconFactory(
+    <path
+        fill="#529BBA"
+        d="M10.3 23.3l.8-4H8.6v-2.1h3l.5-2.5H9.5v-2.1h3.1l.8-3.9h2.8l-.8 3.9h2.8l.8-3.9h2.8l-.8 3.9h2.5v2.1h-2.9l-.6 2.5h2.6v2.1h-3l-.8 4H16l.8-4H14l-.8 4h-2.9zm6.9-6.1l.5-2.5h-2.8l-.5 2.5h2.8z"
+    />,
+    'CssIcon',
+    32,
+    32
+);
+
+CssIcon.displayName = 'CssIcon';
