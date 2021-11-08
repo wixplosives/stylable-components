a2327b023549ea474188bbf814145881dcbc3f87
Nadav Abrahami <nadav_ab@wix.com>
2021-11-08 15:23:05 +0200

    work on css variables

--- packages/components/src/auto-complete/auto-complete.sim.tsx
+++ packages/components/src/auto-complete/auto-complete.sim.tsx
@@ -3,9 +3,6 @@ import { ZeeRoot } from '../simulation-mixins/mixin-zee-root';
 import { AutoComplete } from './auto-complete';
 import { mixinProjectThemes } from '../simulation-mixins/mixin-project-themes';
 import { ItemData, ItemRenderer } from '../simulation-assets/item-renderer';
-import { Root } from '@zeejs/react';
-import { classes } from '../themes/white.st.css';
-import React from 'react';
 
 const items = new Array(30000).fill(0).map(
     (_, idx) =>
@@ -22,33 +19,6 @@ export default createSimulation<AutoComplete<ItemData, HTMLElement>>({
         items,
         getId: (item: ItemData) => item.id,
         getTextContent: (item: ItemData) => item.title,
-    },
-    wrapper: ({ renderSimulation }) => {
-        return (
-            <div className={classes.white}>
-                <Root
-                    style={{
-                        width: '100vw',
-                        height: '100vh',
-                        position: 'fixed',
-                        top: '0px',
-                        left: '0px',
-                    }}
-                >
-                    <div
-                        style={{
-                            width: '250px',
-                            height: '40px',
-                            position: 'absolute',
-                            top: 'calc( 50vh - 20px )',
-                            left: 'calc( 50vw - 125px )',
-                        }}
-                    >
-                        {renderSimulation()}
-                    </div>
-                </Root>
-            </div>
-        );
     },
     plugins: [ZeeRoot, mixinProjectThemes],
     environmentProps: {
