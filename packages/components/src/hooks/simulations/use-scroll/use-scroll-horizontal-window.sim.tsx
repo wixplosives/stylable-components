da5a0a5b07896f0a199a0ff8125d0fed9b2e1caa
eifr <ofirl@wix.com>
2021-12-08 11:57:33 +0200

    duplicate action

--- packages/components/src/hooks/simulations/use-scroll/use-scroll-horizontal-window.sim.tsx
+++ packages/components/src/hooks/simulations/use-scroll/use-scroll-horizontal-window.sim.tsx
@@ -26,6 +26,7 @@ export default createSimulation({
             title: 'scrolls horizontally',
             events: [
                 scrollAction(200, false),
+                scrollAction(200, false),
                 expectElementText('#res', '200'),
                 scrollAction(-1, false),
                 expectElement('#res', (el: HTMLElement) => {
