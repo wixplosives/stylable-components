b9b852627a8f0856902ea348a245b1bf2871fc84
Tetiana Mandziuk <tetianaman@wix.com>
2021-12-15 17:34:29 +0200

    redundant files

--- special components/toggle-switch/toggle-switch.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-import { style, classes } from './toggle-switch.st.css';
-
-export type ToggleSwitchProps = React.HTMLAttributes<HTMLDivElement>;
-
-export const ToggleSwitch: FC<ToggleSwitchProps> = (props) => {
-    return <div {...props} className={style(classes.root, props.className)} />;
-};
