bf923ce9b1f8062ed27a023aaf8912b4c0a1c889
Peter Shershov <peters@wix.com>
2022-05-18 14:56:14 +0300

    remove scenario

--- packages/components/src/scroll-list/simulations/scroll-list-with-scroll-position.board.tsx
+++ packages/components/src/scroll-list/simulations/scroll-list-with-scroll-position.board.tsx
@@ -1,13 +1,5 @@
 import React, { useState } from 'react';
 import { ItemData, ItemRenderer } from '../../simulation-assets/item-renderer';
-import {
-    clickAction,
-    expectElementsStyle,
-    expectElementStyle,
-    hoverAction,
-    scenarioMixin,
-    scrollAction,
-} from '../../simulation-mixins/scenario';
 import { ScrollList } from '../scroll-list';
 import { mixinProjectThemes } from '../../simulation-mixins/mixin-project-themes';
 import { createBoard } from '@wixc3/react-board';
@@ -57,32 +49,5 @@ export default createBoard({
         windowHeight: 300,
         windowWidth: 600,
     },
-    plugins: [
-        scenarioMixin.use({
-            skip: true,
-            events: [
-                hoverAction('[data-id="a8"]'),
-                expectElementStyle('[data-id="a8"]', {
-                    color: 'rgb(0, 0, 255)',
-                }),
-                hoverAction('[data-id="a9"]'),
-                expectElementsStyle({
-                    '[data-id="a8"]': {
-                        color: 'rgb(0, 0, 0)',
-                    },
-                    '[data-id="a9"]': {
-                        color: 'rgb(0, 0, 255)',
-                    },
-                }),
-                clickAction('[data-id="a9"]'),
-                expectElementStyle('[data-id="a9"]', {
-                    backgroundColor: 'rgb(0, 0, 255)',
-                }),
-                clickAction('[data-id="a11"]'),
-                scrollAction(-1),
-                scrollAction(0),
-            ],
-        }),
-        mixinProjectThemes,
-    ],
+    plugins: [mixinProjectThemes],
 });
