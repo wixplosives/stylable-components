74613fb63260c8b767636e0a31a64513e91a3f3e
Nadav Abrahami <nadav_ab@wix.com>
2022-08-28 12:54:53 +0300

    fixed test

--- packages/components/src/scroll-list/simulations/element-scroll-items-scrolloffset.board.tsx
+++ packages/components/src/scroll-list/simulations/element-scroll-items-scrolloffset.board.tsx
@@ -46,10 +46,11 @@ export default createBoard({
                 ref={ref}
             >
                 <div>
-                    <h1
+                    <div
                         style={{
                             height: `${offsetHeight}px`,
                         }}
+                        id="offset"
                     >
                         <input
                             value={offsetHeight}
@@ -57,7 +58,7 @@ export default createBoard({
                             onChange={(ev) => setOffsetHeight(parseInt(ev.target.value))}
                             id="input"
                         ></input>
-                    </h1>
+                    </div>
                     <ScrollList
                         ItemRenderer={ItemRenderer}
                         items={items}
@@ -90,10 +91,16 @@ export default createBoard({
     },
     plugins: [
         scenarioMixin.use({
-            title: 'should listen to item resize',
+            title: 'should listen to offset resize',
             timeout: 10_000,
             events: [
+                expectElements(['#offset', '[data-id="a0"]'], (els) => {
+                    const rootBox = els['#offset'].getBoundingClientRect();
+                    const firstItem = els['[data-id="a0"]'].getBoundingClientRect();
+                    expect(Math.ceil(firstItem.top)).to.be.equal(Math.ceil(rootBox.bottom));
+                }),
                 scrollAction(-1, true, '#scroll-root'),
+
                 expectElements(['#scroll-root', '[data-id="a999"]'], (els) => {
                     const rootBox = els['#scroll-root'].getBoundingClientRect();
                     const lastItem = els['[data-id="a999"]'].getBoundingClientRect();
