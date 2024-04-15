235baa79daa2e31d86c5d6bcbbe373e67ea747ad
Avi Vahl <avi.vahl@wix.com>
2024-04-15 19:26:30 +0300

    infra: add stylable config; remove lerna

--- stylable.config.js
+++ stylable.config.js
@@ -1,7 +1,20 @@
-module.exports = {
-    defaultConfig() {
-        return {
-            experimentalSelectorInference: true,
-        };
+// @ts-check
+
+const { stcConfig } = require('@stylable/cli');
+
+exports.stcConfig = stcConfig({
+    options: {
+        srcDir: './src',
+        outDir: './dist',
+        outputSources: true,
+        cjs: false,
+        useNamespaceReference: true,
     },
+    projects: ['packages/*'],
+});
+
+exports.defaultConfig = () => {
+    return {
+        experimentalSelectorInference: true,
+    };
 };
